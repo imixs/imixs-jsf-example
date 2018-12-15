e6b034b7b2eaa3fd7e433e9da5eeede385c7fff3
Ralph Soika <ralph.soika@imixs.com>
2018-12-15 12:12:18 +0100

    refactoring
    issue #28

--- src/main/java/org/imixs/application/ui/TeamlistController.java
+++ src/main/java/org/imixs/application/ui/TeamlistController.java
@@ -9,23 +9,26 @@ import javax.inject.Named;
 
 import org.imixs.workflow.faces.data.ViewController;
 
+/**
+ * Select the list of teams sorted by name
+ * 
+ * @author rsoika
+ */
 @Named
 @ViewScoped
-public class WorklistController extends ViewController implements Serializable {
+public class TeamlistController extends ViewController implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@SuppressWarnings("unused")
-	private static Logger logger = Logger.getLogger(WorklistController.class.getName());
+	private static Logger logger = Logger.getLogger(TeamlistController.class.getName());
 
 	@Override
 	@PostConstruct
 	public void init() {
 		super.init();
-
-		this.setType("workitem");
-		this.setQuery("(type:\"workitem\")");
-		this.setSortBy("$modified");
-		this.setSortReverse(true);
+		this.setQuery("(type:\"team\")");
+		this.setSortBy("txtname");
+		this.setSortReverse(false);
 	}
 
 }
